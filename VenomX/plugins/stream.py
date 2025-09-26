c39f3e7168f87b2af80dbc5bd121db7fcfa88486
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-09-26 21:47:43 +0530

    Update stream.py

--- VenomX/plugins/stream.py
+++ VenomX/plugins/stream.py
@@ -5,8 +5,6 @@ from VenomX import add_to_queue
 from VenomX import download_media_file
 from VenomX import get_media_info, get_media_stream
 from pyrogram import filters
-from pytgcalls.exceptions import AlreadyJoinedError, GroupCallNotFound
-from pytgcalls.exceptions import NoActiveGroupCall
 
 
 @app.on_message(cdz(["ply", "play", "vply", "vplay"]) & ~filters.private)
@@ -46,6 +44,8 @@ async def start_stream(client, message):
         else:
             type = "Audio"
         media = await download_media_file(link, type)
+
+    # ---------------- Updated Error Handling ----------------
     try:
         a = await call.get_call(chat_id)
         if a.status == "not_playing":
@@ -53,21 +53,15 @@ async def start_stream(client, message):
             await call.change_stream(chat_id, stream)
             await add_to_queue(chat_id, media=media, type=type)
             return await aux.edit("**Streaming Started ....**")
-        elif (a.status == "playing" or a.status == "paused"):
+        elif a.status in ("playing", "paused"):
             position = await add_to_queue(chat_id, media=media, type=type)
             return await aux.edit(f"**Added to Queue At {position}**")
-    except GroupCallNotFound:
+    except Exception as e:
         try:
             stream = await get_media_stream(media, type)
             await call.join_group_call(chat_id, stream, auto_start=False)
             await add_to_queue(chat_id, media=media, type=type)
             return await aux.edit("**Streaming Started ....**")
-        except NoActiveGroupCall:
-            return await aux.edit("**No Active VC !**")
-        except AlreadyJoinedError:
-            return await aux.edit("**Assistant Already in VC !**")
         except Exception as e:
             print(f"Error: {e}")
             return await aux.edit("**Please Try Again !**")
-        except:
-            return
