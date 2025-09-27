c03b1777228e0121337e0aca4ecb161ae7f0bb22
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-09-27 13:53:35 +0530

    Update __init__.py

--- VenomX/modules/__init__.py
+++ VenomX/modules/__init__.py
@@ -1,5 +1,5 @@
 from .clients import app, bot, call
-from .events import cdx, cdz, eor, call_decorators
+from .events import cdx, cdz, eor
 from .helpers import download_media_file
 from .queues import add_to_queue, get_from_queue
 from .queues import is_queue_empty, task_done, clear_queue
