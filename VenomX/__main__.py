dff36afc60ec936641d5397309d2ebcfae01ad77
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-09-27 13:38:22 +0530

    Update __main__.py

--- VenomX/__main__.py
+++ VenomX/__main__.py
@@ -1,28 +1,32 @@
-import asyncio
-from pyrogram import idle
+import asyncio, pyrogram
+
 from VenomX import app, bot, call
+from VenomX import call_decorators
 from VenomX.plugins import load_plugins
-from VenomX.modules.events import call_decorators
+
 
 loop = asyncio.get_event_loop()
 
 async def init():
-    print("🔹 Starting clients...")
-    await app.start()
-    print("✅ User client started")
-    await bot.start()
-    print("✅ Bot client started")
-    await call.start()
-    print("✅ PyTgCalls client started")
-    await call_decorators()
-    print("✅ Stream handlers registered")
-    await load_plugins()
-    print("✅ All plugins loaded")
-    print("🎵 Userbot Started")
-    await idle()
+    print("Starting all clients ...")
+    try:
+        await app.start()
+        print("User client started.")
+        await bot.start()
+        print("Bot client started.")
+        await call.start()
+        print("PyTgCalls client started.")
+        await load_plugins()
+        print("All plugins loaded.")
+        await call_decorators()
+    except Exception as e:
+        return print(
+            f"Error: {e}"
+        )
+    print("VenomX Now Started !!")
+    await pyrogram.idle()
+
 
 if __name__ == "__main__":
-    try:
-        loop.run_until_complete(init())
-    except (KeyboardInterrupt, SystemExit):
-        print("⚡ Userbot stopped")
+    loop.run_until_complete(init())
+    print("VenomX Now Stopped !!")
