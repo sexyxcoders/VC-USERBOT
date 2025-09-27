3fe0ea2a193fd66f743ddfe79dab570ab15681af
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-09-27 14:09:05 +0530

    Update helpers.py

--- VenomX/modules/helpers.py
+++ VenomX/modules/helpers.py
@@ -1,4 +1,4 @@
-import asyncio, os, yt_dlp
+import asyncio, os, yt_dlp, certifi
 
 
 async def download_media_file(link: str, type: str):
@@ -8,10 +8,11 @@ async def download_media_file(link: str, type: str):
             "format": "bestaudio/best",
             "outtmpl": "downloads/%(id)s.%(ext)s",
             "geo_bypass": True,
-            "nocheckcertificate": True,
+            "nocheckcertificate": False,   # use proper certs instead of disabling
             "quiet": True,
             "no_warnings": True,
             "cookiefile": "cookies.txt",
+            "ca_certs": certifi.where(),   # SSL fix
         }
 
     elif type == "Video":
@@ -19,10 +20,11 @@ async def download_media_file(link: str, type: str):
             "format": "(bestvideo[height<=?720][width<=?1280][ext=mp4])+(bestaudio[ext=m4a])",
             "outtmpl": "downloads/%(id)s.%(ext)s",
             "geo_bypass": True,
-            "nocheckcertificate": True,
+            "nocheckcertificate": False,   # use proper certs instead of disabling
             "quiet": True,
             "no_warnings": True,
             "cookiefile": "cookies.txt",
+            "ca_certs": certifi.where(),   # SSL fix
         }
         
     x = yt_dlp.YoutubeDL(ydl_opts)
