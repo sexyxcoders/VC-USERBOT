0bdf1d43a636181af7a47cd035418f9f494b7b2d
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-09-27 09:54:57 +0530

    Update events.py

--- VenomX/modules/events.py
+++ VenomX/modules/events.py
@@ -1,11 +1,11 @@
 from typing import Union, List
 from pyrogram import filters
 from pyrogram.types import Message
-from VenomX import config
-from VenomX.modules import queues
+from pytgcalls import StreamEnd
 from VenomX.modules.clients import app, call
 from VenomX.modules.streams import get_media_stream
-from pytgcalls import StreamEnd
+from VenomX.modules import queues
+from VenomX import config
 
 # Command filters
 def cdx(commands: Union[str, List[str]]):
@@ -14,27 +14,24 @@ def cdx(commands: Union[str, List[str]]):
 def cdz(commands: Union[str, List[str]]):
     return filters.command(commands, config.COMMAND_HANDLERS)
 
-# Edit or reply helper
-async def eor(message: Message, *args, **kwargs) -> Message:
-    msg = message.edit_text if getattr(message.from_user, "is_self", False) or getattr(message, "outgoing", False) else (message.reply_to_message or message).reply_text
+# Edit or reply
+async def eor(message: Message, *args, **kwargs):
+    msg = message.edit_text if getattr(message, "outgoing", False) else (message.reply_to_message or message).reply_text
     return await msg(*args, **kwargs)
 
-# Stream end handler registration
+# Stream-end handler
 async def call_decorators():
     @call.on_stream_end()
     async def stream_end_handler(update: StreamEnd):
         chat_id = update.chat_id
         await queues.task_done(chat_id)
-        queue_empty = await queues.is_queue_empty(chat_id)
-        if queue_empty:
+        if await queues.is_queue_empty(chat_id):
             try:
                 await call.leave_group_call(chat_id)
             except:
                 return
         else:
             check = await queues.get_from_queue(chat_id)
-            media = check["media"]
-            type_ = check["type"]
-            stream = await get_media_stream(media, type_)
+            stream = await get_media_stream(check["media"], check["type"])
             await call.change_stream(chat_id, stream)
             await app.send_message(chat_id, "🎵 Now Streaming ...")
