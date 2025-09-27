e6465f1402b405110376a6f091843a4d98d960fa
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-09-27 09:49:44 +0530

    Update streams.py

--- VenomX/modules/streams.py
+++ VenomX/modules/streams.py
@@ -1,27 +1,23 @@
 import asyncio, yt_dlp
 from typing import Union
 from pyrogram.types import Audio, Voice, Video, VideoNote
-from pytgcalls.types import AudioQuality, VideoQuality, MediaStream
+from pytgcalls.types.input_stream import InputAudioStream, InputVideoStream
+from pytgcalls.types.input_stream.quality import AudioQuality, VideoQuality
 from youtubesearchpython.__future__ import VideosSearch
 from VenomX import config
 
-# Get audio file name
 def get_audio_name(audio: Union[Audio, Voice]):
     try:
-        file_name = audio.file_unique_id + "." + (audio.file_name.split(".")[-1] if not isinstance(audio, Voice) else "ogg")
+        return audio.file_unique_id + "." + (audio.file_name.split(".")[-1] if not isinstance(audio, Voice) else "ogg")
     except:
-        file_name = audio.file_unique_id + ".ogg"
-    return file_name
+        return audio.file_unique_id + ".ogg"
 
-# Get video file name
 def get_video_name(video: Union[Video, VideoNote]):
     try:
-        file_name = video.file_unique_id + "." + video.file_name.split(".")[-1]
+        return video.file_unique_id + "." + video.file_name.split(".")[-1]
     except:
-        file_name = video.file_unique_id + ".mp4"
-    return file_name
+        return video.file_unique_id + ".mp4"
 
-# Get YouTube video info
 async def get_media_info(vidid: str, query: str):
     url = f"https://www.youtube.com/watch?v={vidid}" if vidid else None
     search = url if url else query
@@ -31,7 +27,6 @@ async def get_media_info(vidid: str, query: str):
         videourl = url if vidid else result["link"]
     return [videoid, videourl]
 
-# Direct YouTube stream link
 async def get_stream_link(link: str):
     proc = await asyncio.create_subprocess_exec(
         "yt-dlp", "-g", "-f", "bestvideo+bestaudio/best",
@@ -43,17 +38,14 @@ async def get_stream_link(link: str):
     links = stdout.decode().split("\n")
     return links[0], links[1]
 
-# Create MediaStream
 async def get_media_stream(media, type_: str):
     if type_ == "Audio":
-        return MediaStream(
-            media_path=media,
-            video_flags=MediaStream.IGNORE,
-            audio_parameters=AudioQuality.STUDIO
+        return InputAudioStream(
+            media,
+            quality=AudioQuality.STUDIO
         )
     elif type_ == "Video":
-        return MediaStream(
-            media_path=media,
-            audio_parameters=AudioQuality.STUDIO,
-            video_parameters=VideoQuality.HD_720p
+        return InputVideoStream(
+            media,
+            quality=VideoQuality.HD_720p
         )
